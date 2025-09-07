b33a96504cced6af0943197beaf3080fb3563657
Mohaiminul Islam <mohaiminulislam@Mohaiminuls-MacBook-Air.local>
2025-09-07 22:11:07 +0200

    Higher lower game

---
+++ Day_14/main.py
@@ -0,0 +1,47 @@
+from art import logo
+from art import vs
+from random import randint
+from game_data import data
+
+
+
+
+def higher_lower_game():
+    print(logo)
+    score=0
+    game_should_continue=True
+    i=randint(0,len(data)-1)
+    A=data[i]
+    
+    
+
+    while game_should_continue:
+        
+        
+        print(f"Compare A: {A['name']},a {A['description']},from {A['country']}.")
+        print(vs)
+        
+        j=i
+        while j==i:
+            j=randint(0,len(data)-1)
+        B=data[j]
+        print(f"Compare B:{B['name']},a {B['description']},from {B['country']}.")
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if (guess=="a" and A['follower_count'] > B['follower_count']) or (guess=="b" and B['follower_count'] > A['follower_count']):
+            score+=1
+            print(f"your current score {score}")
+            if guess=="a":
+            
+                A=data[i]
+            else:
+                A=B
+                
+        else:
+            print(f"you have lost. your score {score}")
+            game_should_continue=False
+
+        
+
+
+
+higher_lower_game()
